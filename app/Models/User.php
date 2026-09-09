7516ea79b870f264fda03d0cee8e4b2da78d0e02
DEVEduardoiensen <dev.eduardoiensen@gmail.com>
2026-09-09 17:16:25 -0300

    ATV 20: Adicionar campo role com tipos diferentes no User

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,7 +10,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-#[Fillable(['name', 'email', 'password'])]
+#[Fillable(['name', 'email', 'password', 'role'])]
 #[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable
 {
@@ -36,5 +36,18 @@ class User extends Authenticatable
     public function aluno()
     {
         return $this->hasOne(Aluno::class, 'user_id');
+    }
+
+    /**
+     * ATV 20: Métodos auxiliares para verificação dos tipos de perfil (Role)
+     */
+    public function isAdmin(): bool
+    {
+        return $this->role === 'admin';
+    }
+
+    public function isProfessor(): bool
+    {
+        return $this->role === 'professor';
     }
 }
