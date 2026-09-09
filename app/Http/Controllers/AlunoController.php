94dac43890c0c330a4c39fa234330b671a9f89c1
DEVEduardoiensen <dev.eduardoiensen@gmail.com>
2026-09-08 21:42:03 -0300

    ATV 4: Implementar os 7 métodos principais de CRUD no AlunoController e rotas

--- app/Http/Controllers/AlunoController.php
+++ app/Http/Controllers/AlunoController.php
@@ -6,5 +6,48 @@ use Illuminate\Http\Request;
 
 class AlunoController extends Controller
 {
-    //
+    // ATV 4: Implementação dos 7 métodos principais de CRUD
+
+    // 1. Listar todos os alunos
+    public function index()
+    {
+        return 'Página de Listagem de Alunos (index)';
+    }
+
+    // 2. Formulário para criar novo aluno
+    public function create()
+    {
+        return 'Formulário de Cadastro de Aluno (create)';
+    }
+
+    // 3. Salvar novo aluno no banco de dados
+    public function store(Request $request)
+    {
+        return 'Processando cadastro do aluno (store)';
+    }
+
+    // 4. Exibir detalhes de um aluno específico
+    public function show(string $id)
+    {
+        return "Exibindo detalhes do Aluno ID: {$id} (show)";
+    }
+
+    // 5. Formulário para editar um aluno existente
+    public function edit(string $id)
+    {
+        return "Formulário de Edição do Aluno ID: {$id} (edit)";
+    }
+
+    // 6. Atualizar os dados do aluno no banco de dados
+    public function update(Request $request, string $id)
+    {
+        return "Atualizando dados do Aluno ID: {$id} (update)";
+    }
+
+    // 7. Excluir um aluno do banco de dados
+    public function destroy(string $id)
+    {
+        return "Excluindo Aluno ID: {$id} (destroy)";
+    }
 }
+
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AlunoController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -11,13 +12,13 @@ Route::get('/sobre', function () {
     return 'Página Sobre: Informações do sistema';
 });
 
-Route::get('/alunos', function () {
-    return 'Página de Alunos: Lista de alunos cadastrados';
-});
-
 Route::get('/contato', function () {
     return 'Página de Contato: Entre em contato conosco';
 });
+
+// TEMA 2 / ATV 4: 7 rotas principais de CRUD com AlunoController
+Route::resource('alunos', AlunoController::class);
+
 
 // ATV 2: Rotas com parâmetro retornando texto
 Route::get('/produto/{id}', function ($id) {
