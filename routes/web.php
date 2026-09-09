7da913ff8c539cca6de0401329ff51a2f7479842
DEVEduardoiensen <dev.eduardoiensen@gmail.com>
2026-09-08 21:40:59 -0300

    ATV 1: Criar rotas /sobre, /alunos e /contato com retorno de texto

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,17 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// ATV 1: Rotas simples retornando texto
+Route::get('/sobre', function () {
+    return 'Página Sobre: Informações do sistema';
+});
+
+Route::get('/alunos', function () {
+    return 'Página de Alunos: Lista de alunos cadastrados';
+});
+
+Route::get('/contato', function () {
+    return 'Página de Contato: Entre em contato conosco';
+});
+
